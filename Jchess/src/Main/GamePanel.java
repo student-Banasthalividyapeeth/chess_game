b9bd01ac828095b05d1148ae21202525e60c4303
student-Banasthalividyapeeth <manshisingh1208@gmail.com>
2024-08-06 20:31:12 +0530

    added run and thread method

--- Jchess/src/Main/GamePanel.java
+++ Jchess/src/Main/GamePanel.java
@@ -2,18 +2,27 @@ package main;
 import java.awt.*;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel
+public class GamePanel extends JPanel implements Runnable
 {
     public static final int width = 1100;
     public static final int height = 800;
+    final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel()
     {
         setPreferredSize(new Dimension(width, height));
         setBackground(Color.BLACK);
     }
+    public void launchGame(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    public void run(){
+
+    }
     private void update(){
-        
+
     }
     @Override
     public void paintComponent(Graphics g)
@@ -22,4 +31,5 @@ public class GamePanel extends JPanel
         g.drawString("This is my custom pamnel!!", 10, 20);
 
     }
+
 }
